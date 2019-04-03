a737d3b3d4e839e07acfd669dbba78cf4b52c386
Pimenova Maya <may-goldenvol@yandex.ru>
2019-04-03 13:50:03 +0300

    replace function 'greeting' with 'playEvenGame'

--- src/Even.php
+++ src/Even.php
@@ -5,10 +5,32 @@
     use function \cli\line;
     use function \cli\prompt;
 
-function greeting()
+function playEvenGame()
 {
     line("Welcome to the Brain Games!");
     line("Answer \"yes\" if number even otherwise answer \"no\".\n");
     $name = prompt("May I have your name?");
     line("Hello, %s!", $name);
+
+    function isEven($num)
+    {
+        return $num % 2 === 0;
+    }
+
+    for ($i = 0; $i < 3; $i++) {
+        $number = rand(0, 100);
+        line("Question: %s", $number);
+        $answer = prompt("Your answer");
+
+        $correctAnswer = isEven($number) ? 'yes' : 'no';
+
+        if ($answer !== $correctAnswer) {
+            line(" '%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $correctAnswer);
+            line("Let's try again, %s!", $name);
+            break;
+        } elseif ($i === 2) {
+            line("Correct!");
+            line("Congratulations, %s!", $name);
+        }
+    }
 }
