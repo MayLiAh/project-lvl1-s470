9e0045de340097c1f091a48fc13dab038c1d749c
Pimenova Maya <may-goldenvol@yandex.ru>
2019-04-04 14:23:54 +0300

    modified Cli.php, Even.php, Calc.php

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,27 @@
     use function \cli\line;
     use function \cli\prompt;
 
-function run()
+function run($gameInstruction, $gameQuestion, $correctAnswer)
 {
     line("Welcome to the Brain Game!\n");
+    line("%s", $gameInstruction);
     $name = prompt("May I have your name?");
-    line("Hello, %s!", $name);
+    line("Hello, %s!\n", $name);
+    $answersToWin = 3;
+
+    for ($i = 0; $i < $answersToWin; $i++) {
+        $question = $gameQuestion();
+        line("Question: %s", $question);
+        $answer = prompt("Your answer");
+
+        if ($answer != $correctAnswer($question)) {
+            line(" '%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $correctAnswer($question));
+            line("Let's try again, %s!", $name);
+            return false;
+        } else {
+            line("Correct!");
+        }
+    }
+
+    line("Congratulations, %s!", $name);
 }
