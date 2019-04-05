72b91c0e013d3dd0a30fb223bcce4a1fe6e7d9e4
Pimenova Maya <may-goldenvol@yandex.ru>
2019-04-05 13:26:46 +0300

    modified Calc.php

--- src/games/Calc.php
+++ src/games/Calc.php
@@ -6,15 +6,15 @@
 
 
     const GAME_INSTRUCTION = "What is the result of the expression?\n";
+    const OPERATORS = ['+', '-', '*'];
 
 function playCalcGame()
 {
     $gameQuestion = function () {
-        $operators = ['+', '-', '*'];
         $number1 = rand(0, 100);
         $number2 = rand(0, 100);
-        $operatorIndex = rand(0, 2);
-        $currentOperator = $operators[$operatorIndex];
+        $operatorIndex = rand(0, count(OPERATORS) - 1);
+        $currentOperator = OPERATORS[$operatorIndex];
         $symbols = [$number1, $currentOperator, $number2];
         return implode(' ', $symbols);
     };
