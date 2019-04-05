127b414364d40db9dad1a83de6b007eeaff6704f
Pimenova Maya <may-goldenvol@yandex.ru>
2019-04-05 18:21:47 +0300

    modified Progression game

--- src/games/Progression.php
+++ src/games/Progression.php
@@ -1,31 +1,40 @@
 <?php
 
-    namespace BrainGames\Games\Progression;
+namespace BrainGames\Games\Progression;
 
-    use function BrainGames\Cli\run;
+use function BrainGames\Engine\run;
 
+const GAME_INSTRUCTION = "What number is missing in the progression?\n";
+const PROGRESSION_STEP = 6;
+const PROGRESSION_LENGTH = 10;
+const MAX_START = 100 - PROGRESSION_STEP * 10;
 
-    const GAME_INSTRUCTION = "What number is missing in the progression?\n";
-    const PROGRESSION = [100, 94, 88, 82, 76, 70, 64, 58, 52, 46];
+function playProgressionGame()
+{
+    $gameSymbols = function () {
+        $progressionStart = rand(0, MAX_START);
+        $currentProgression = [];
+        for ($i = 0; $i < PROGRESSION_LENGTH; $i++) {
+            $currentProgression[] = $progressionStart;
+            $progressionStart += PROGRESSION_STEP;
+        }
+        $numberIndex = array_rand($currentProgression);
+        $currentProgression[$numberIndex] = '..';
+        return $currentProgression;
+    };
 
-    function playProgressionGame()
-    {
-        $gameSymbols = function () {
-            $numberIndex = rand(0, count(PROGRESSION) - 1);
-            $currentProgression = PROGRESSION;
-            $currentProgression[$numberIndex] = '..';
-            return $currentProgression;
-        };
+    $correctAnswer = function ($symbols) {
+        $numberKey = array_search('..', $symbols);
+        if ($numberKey !== count($symbols) - 1) {
+            return $symbols[$numberKey + 1] - PROGRESSION_STEP;
+        }
+        return $symbols[$numberKey - 1] + PROGRESSION_STEP;
+    };
 
-        $correctAnswer = function ($symbols) {
-            $numberKey = array_search('..', $symbols);
-            return PROGRESSION[$numberKey];
-        };
+    $gameBody = [
+                 'symbols' => $gameSymbols,
+                 'answer' => $correctAnswer
+                ];
 
-        $gameBody = [
-                     'symbols' => $gameSymbols,
-                     'answer' => $correctAnswer
-                    ];
-
-        run(GAME_INSTRUCTION, $gameBody);
-    }
+    run(GAME_INSTRUCTION, $gameBody);
+}
