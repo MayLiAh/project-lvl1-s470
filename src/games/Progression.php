ac15e428149386f663e93048462c47a018630705
Pimenova Maya <may-goldenvol@yandex.ru>
2019-04-07 13:32:55 +0300

    add reverse of progression in brain-progression

--- src/games/Progression.php
+++ src/games/Progression.php
@@ -7,6 +7,15 @@ use function BrainGames\Engine\run;
 const GAME_INSTRUCTION = 'What number is missing in the progression?';
 const LENGTH = 10;
 
+function randomReverseOfProgression($progression)
+{
+    $reverseProgression = array_rand([0 => 'no', 1 => 'yes']);
+    if ($reverseProgression === 0) {
+        return $progression;
+    }
+    return array_reverse($progression);
+}
+
 function generateProgression($start, $step)
 {
     $progression = [];
@@ -17,16 +26,15 @@ function generateProgression($start, $step)
     return $progression;
 }
 
-
 function playProgressionGame()
 {
     $generateQuestionAndAnswer = function () {
-        $step = rand(-7, 7);
-        $start = rand(-30, 30);
+        $step = rand(2, 7);
+        $start = rand(0, 30);
         $progression = generateProgression($start, $step);
         $indexOfHiddenNumber = array_rand($progression);
         $progression[$indexOfHiddenNumber] = '..';
-        $question = implode(' ', $progression);
+        $question = implode(' ', randomReverseOfProgression($progression));
         $answer = $start + $step * $indexOfHiddenNumber;
         return [$question, $answer];
     };
